44ebbb57a7647705d4b28031fd37a04a84435eed
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-10-28 22:13:30 +0530

    Update Plagarism_Checker.py

--- Plagarism_Checker.py
+++ Plagarism_Checker.py
@@ -3,6 +3,8 @@
 import nltk  # natural language toolkit used for tasks related to natural language processing
 # nltk.download("popular")  # Downloads popular datasets and pre-trained models for NLP tasks
 import pandas as pd
+import string
+from nltk.corpus import stopwords
 
 # Load Dataset
 data=pd.read_csv("C:\\TrueScript\\dataset.csv")
@@ -10,3 +12,19 @@ data=pd.read_csv("C:\\TrueScript\\dataset.csv")
 
 # Checking distribution of label function
 print(data['label'].value_counts())
+
+# Clean Text
+def preprocess_text(text):
+    # Remove punctuation
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    # Convert to lowercase
+    text = text.lower()
+    # Remove stop words
+    stop_words = set(stopwords.words("english"))
+    text = " ".join(word for word in text.split() if word not in stop_words)
+    return text
+data["source_text"] = data["source_text"].apply(preprocess_text)
+data["plagiarized_text"] = data["plagiarized_text"].apply(preprocess_text)
+
+print(data['source_text'])
+print(data["plagiarized_text"])
