0bc193f8d7749d3ea912124d8c9d03e3e1cd8869
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-10-28 22:47:16 +0530

    Update Plagarism_Checker.py

--- Plagarism_Checker.py
+++ Plagarism_Checker.py
@@ -6,6 +6,9 @@ import pandas as pd
 import string
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split    
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report,confusion_matrix
 
 # Load Dataset
 data=pd.read_csv("C:\\TrueScript\\dataset.csv")
@@ -34,5 +37,27 @@ tfidf_vectorizer = TfidfVectorizer()
 X = tfidf_vectorizer.fit_transform(data["source_text"] + " " + data["plagiarized_text"])
 y = data["label"]
 
-print(X)
-print(y)
+# print(X)
+# print(y)
+
+# Train Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# Logistic Regression
+
+model = LogisticRegression()
+model.fit(X_train,y_train)
+
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+classification_rep = classification_report(y_test, y_pred)
+
+cm = confusion_matrix(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Classification Report:")
+print(classification_rep)
+print("Confusion Matrix")
+print(cm)
+
