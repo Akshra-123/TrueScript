de7142f05ef51661a793f60867c0cfa48e6ecf23
AKSHRA_0304 <153852401+Akshra-123@users.noreply.github.com>
2024-10-28 22:51:50 +0530

    Update Plagarism_Checker.py

--- Plagarism_Checker.py
+++ Plagarism_Checker.py
@@ -11,6 +11,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report,confusion_matrix
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import SVC
 
 # Load Dataset
 data=pd.read_csv("C:\\TrueScript\\dataset.csv")
@@ -87,6 +88,26 @@ print(cm)
 # Naive Bayes 
 # Instantiate the model
 model = MultinomialNB()
+# Fit the model
+model.fit(X_train, y_train)
+# Make predictions
+y_pred = model.predict(X_test)
+# Calculate accuracy
+accuracy = accuracy_score(y_test, y_pred)
+# Generate classification report
+classification_rep = classification_report(y_test, y_pred)
+# Generate confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+# Print results
+print("Accuracy:", accuracy)
+print("Classification Report:")
+print(classification_rep)
+print("Confusion Matrix:")
+print(cm)
+
+# SVM
+# Instantiate the model
+model = SVC(kernel='linear', random_state=42)
 # Fit the model
 model.fit(X_train, y_train)
 # Make predictions
